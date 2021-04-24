389c6363febbab592de8c9a30fa6543a968db062
Ronald MacDonald <ronald@rmacd.com>
2021-04-24 20:21:00 +0100

    fix: use jackson2 to generate .d.ts

--- src/generated/NoticeSearchResponse.d.ts
+++ src/generated/NoticeSearchResponse.d.ts
@@ -1,88 +1,88 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 2.30.840 on 2021-04-24 19:55:19.
+// Generated using typescript-generator version 2.30.840 on 2021-04-24 20:20:29.
 
 export interface NoticeSearchResponse {
-    ByRegion: ByRegion;
-    ByStatus: ByStatus;
-    ByType: ByType;
-    HitCount?: number;
-    MaxHits?: number;
-    NoticeList: NoticeList;
+    byRegion?: ByRegion;
+    byStatus?: ByStatus;
+    byType?: ByType;
+    hitCount?: number;
+    maxHits?: number;
+    noticeList?: NoticeList;
 }
 
 export interface ByRegion {
-    Items: RegionItems;
-    Other?: number;
+    items?: RegionItems;
+    other?: number;
 }
 
 export interface ByStatus {
-    Items: StatusItems;
-    Other?: number;
+    items?: StatusItems;
+    other?: number;
 }
 
 export interface ByType {
-    Items: TypeItems;
-    Other?: number;
+    items?: TypeItems;
+    other?: number;
 }
 
 export interface NoticeList {
-    HitOfNoticeIndex?: NoticeHitType[];
+    hitOfNoticeIndex?: NoticeHitType[];
 }
 
 export interface RegionItems {
-    KeyValuePairOfstringlong?: KVPType[];
+    keyValuePairOfstringlong?: KVPType[];
 }
 
 export interface StatusItems {
-    KeyValuePairOfNullableOfNoticeStatus?: KVPType[];
+    keyValuePairOfNullableOfNoticeStatus?: KVPType[];
 }
 
 export interface TypeItems {
-    KeyValuePairOfNullableOfNoticeType?: KVPType[];
+    keyValuePairOfNullableOfNoticeType?: KVPType[];
 }
 
 export interface NoticeHitType {
-    Score?: number;
-    Item: NoticeIndex;
+    score?: number;
+    item?: NoticeIndex;
 }
 
 export interface KVPType {
-    key: string;
+    key?: string;
     value?: number;
 }
 
 export interface NoticeIndex {
-    Id: string;
-    ParentId?: string;
-    Title?: string;
-    Description?: string;
-    CpvDescription?: string;
-    PublishedDate: Date;
-    DeadlineDate: Date;
-    ApproachMarketDate?: Date;
-    ValueLow: number;
-    ValueHigh: number;
-    Postcode: string;
-    IsSuitableForSme?: boolean;
-    IsSuitableForVco?: boolean;
-    NoticeType?: string;
-    NoticeStatus?: string;
-    Region: string;
-    LastNotifiableUpdate: Date;
-    OrganisationName: string;
-    Sector: string;
-    CpvCodes?: string[];
-    Start: Date;
-    End: Date;
-    NoticeIdentifier: string;
-    CpvDescriptionExpanded?: string;
-    AwardedDate?: Date;
-    AwardedSupplier?: string;
-    AwardedValue: number;
-    Coordinates?: string;
-    IsSubNotice?: boolean;
-    AwardedToSme?: boolean;
-    AwardedToVcse?: boolean;
-    CpvCodesExtended?: string[];
+    id?: string;
+    parentId?: string;
+    title?: string;
+    description?: string;
+    cpvDescription?: string;
+    publishedDate?: Date;
+    deadlineDate?: Date;
+    approachMarketDate?: Date;
+    valueLow?: number;
+    valueHigh?: number;
+    postcode?: string;
+    isSuitableForSme?: boolean;
+    isSuitableForVco?: boolean;
+    noticeType?: string;
+    noticeStatus?: string;
+    region?: string;
+    lastNotifiableUpdate?: Date;
+    organisationName?: string;
+    sector?: string;
+    cpvCodes?: string[];
+    start?: Date;
+    end?: Date;
+    noticeIdentifier?: string;
+    cpvDescriptionExpanded?: string;
+    awardedDate?: Date;
+    awardedSupplier?: string;
+    awardedValue?: number;
+    coordinates?: string;
+    isSubNotice?: boolean;
+    awardedToSme?: boolean;
+    awardedToVcse?: boolean;
+    cpvCodesExtended?: string[];
 }
